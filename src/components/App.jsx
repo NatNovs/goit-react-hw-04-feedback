711ce396c5a64312035d2a2ddab98301f477463e
NatNovs <136241094+NatNovs@users.noreply.github.com>
2024-01-11 15:29:59 +0100

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,46 @@
+import {  useState } from "react";
+import { Statistics } from "./Statistics/Statistics";
+import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
+import { Section } from "./Section/Section";
+import { Notification } from "./Notifications/Notifications";
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 })
+
+  const onBtnClick = e => {
+    const option = e.target.name;
+    setFeedback((prevFeedback) => (
+      { ...prevFeedback, [option]: prevFeedback[option] + 1 }
+    ))
+  }
+  
+  // const totalFeedback = (feedback.good + feedback.neutral + feedback.bad);
+  const totalFeedback = Object.values(feedback).reduce((acc, elem) => (acc + elem), 0) //на случай если будет много элементов в массиве
+  const positivePercentage = totalFeedback>0 ? ((feedback.good / totalFeedback) * 100).toFixed(0) : 0;
+
+    return (
+      <>
+        <Section title="Please leave feedback"> 
+          <FeedbackOptions
+            options={Object.keys(feedback) }
+            onLeaveFeedback={onBtnClick}>
+          </FeedbackOptions>
+        </Section>
+        
+        <Section title="Statistics">
+          {totalFeedback > 0 ?
+            (<Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={totalFeedback}
+              positivePercentage={positivePercentage} />)
+            :
+            (<Notification message='There is no feedback' />)}
+        </Section>
+    
+    </>
   );
 };
